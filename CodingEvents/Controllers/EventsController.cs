1496e8fd2a7edb88fccac698dfd735f76ed6ad3e
Jada <jada.hundley@health.slu.edu>
2023-10-23 20:22:17 -0500

    Added code to the events controller class along with the events view folder

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<string> Events = new List<string>();
+
+            Events.Add("Code With Pride");
+            Events.Add("Strange Loop");
+            Events.Add("Women Who Code");
+
+            ViewBag.events = Events;
+            return View();
+        }
+    }
+}
+
